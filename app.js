92237bc6cba2bb58b4baaf7f8868563de703e438
knightow1 <nick.asmussen1@gmail.com>
2017-05-10 16:20:07 -0700

    finishing for loop seventh question

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ alert('All questions require yes/no or y/n answers.');
 var counter = 0;
 
 while (true) {
-  var music = prompt('First question: Does Nick play Jazz?').toLowerCase();
 
-  console.log('answer: ', music);
+  var music = prompt('First question: Does Nick play Jazz?').toLowerCase();
+  console.log('user music: ', music);
 
   if (music === 'n' || music === 'no') {
     alert('You got it! Great job.');
@@ -23,14 +23,14 @@ while (true) {
 }
 
 while (true){
-  var food = prompt('Second question: Is Nick\'s favorite food tacos?').toLowerCase();
 
-  console.log('answer: ', food);
+  var food = prompt('Second question: Is Nick\'s favorite food tacos?').toLowerCase();
+  console.log('user food: ', food);
 
   if(food === 'n' || food === 'no') {
     alert('Another right answer. You\'re on a roll.');
-    break;
     counter++;
+    break;
   } else if (food === 'y' || food === 'yes') {
     alert('You thought you knew him, I guess not.');
     break;
@@ -40,9 +40,9 @@ while (true){
 }
 
 while (true) {
-  var travel = prompt('Third question: Has Nick been to Burma?').toLowerCase();
 
-  console.log('answer: ', travel);
+  var travel = prompt('Third question: Has Nick been to Burma?').toLowerCase();
+  console.log('user travel: ', travel);
 
   if (travel === 'n' || travel === 'no') {
     alert('Wow you guys must be good friends.');
@@ -57,9 +57,9 @@ while (true) {
 }
 
 while (true) {
-  var color = prompt('Fourth question: Is Nick\'s favorite color green?').toLowerCase();
 
-  console.log('answer: ', color);
+  var color = prompt('Fourth question: Is Nick\'s favorite color green?').toLowerCase();
+  console.log('user color: ', color);
 
   if (color === 'y' || color === 'yes') {
     alert('Good job. Almost done.');
@@ -74,9 +74,9 @@ while (true) {
 }
 
 while (true) {
-  var games = prompt('Fifth question: Does Nick like to play video games?').toLowerCase();
 
-  console.log('answer: ', games);
+  var games = prompt('Fifth question: Does Nick like to play video games?').toLowerCase();
+  console.log('user game: ', games);
 
   if (games === 'y' || games === 'yes') {
     alert('Amazing.');
@@ -91,21 +91,29 @@ while (true) {
 }
 
 alert('The last two questions are not yes/no and you can get multiple guesses.');
-
 var number;
 
 while (number !== 5) {
+
   number = parseInt(prompt('Sixth question: What is his favorite number?'));
+  console.log('user number:', number);
 
   if (number < 5) {
     alert('Too low. Guess again.');
   } else if (number > 5){
     alert('Too high. Guess again.');
-  } else if (number === NaN || number === null) {
+  } else if (isNaN(number) || number === null) {
     alert('Please enter a number.');
   } else {
     alert('Great Job!');
     counter++;
   }
-
 }
+
+alert('This last question has multiple right answers. Final question: Can you guess one of Nick\'s favorite animals?');
+
+var animal = ['dogs', 'owls', 'bears'];
+var answer = prompt('Your choices are cats, dogs, turtles, squids, bears, tigers, monekeys, eagles and owls').toLowerCase();
+var flag;
+
+for (answer)
